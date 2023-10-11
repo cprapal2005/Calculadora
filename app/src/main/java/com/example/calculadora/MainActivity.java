b3be87fd41505629786643c9a838c19a8049d421
cprapal2005 <cprapal2005@g.educaand.es>
2023-10-11 09:12:46 +0200

    Codigo
    Empezar funcionamiento.

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public double numero1=0, numero2=0, resultado=0;
+    public String operacion="", numeroActual="";
+
+    public void resetVariables(View view) {
+
+        numero1=0;
+        numero2=0;
+        resultado=0;
+        operacion="";
+        numeroActual="";
+
+    }
+
+    public void borrarNumero(View view) {
+
+        if(view.getId()==R.id.btnBorrar && numeroActual.length()>0) numeroActual.substring(0, numeroActual.length()-1);
+
+    }
+
+
+
+
+
+
 }
