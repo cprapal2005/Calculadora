738d93a905dabddeeefb8bfe8d5d848db905a7af
cprapal2005 <cprapal2005@g.educaand.es>
2023-10-16 17:40:24 +0200

    95% Hecha
    Buscar Bugs Juan Carlos

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private List<Button> botonesSignos;
     private List<Button> botonesNumeros;
 
+    private Button botonPunto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         signo = "";
 
         numeroActual = findViewById(R.id.txtNumeros);
-
+        botonPunto = findViewById(R.id.btnPunto);
         faseActual = FasesCalculo.DESDE0;
         botonesSignos = Arrays.asList(findViewById(R.id.btnDividir), findViewById(R.id.btnMenos), findViewById(R.id.btnMultiplicar), findViewById(R.id.btnSumar), findViewById(R.id.btnPorcentaje));
         botonesNumeros = Arrays.asList(findViewById(R.id.btn0), findViewById(R.id.btn1), findViewById(R.id.btn2), findViewById(R.id.btn3), findViewById(R.id.btn4), findViewById(R.id.btn5), findViewById(R.id.btn6), findViewById(R.id.btn7), findViewById(R.id.btn8), findViewById(R.id.btn9));
@@ -92,34 +94,55 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textoBoton = (TextView) view;
 
-        if(esNumero(view)==true && faseActual==FasesCalculo.DESDE0) {
+        if(textoBoton.getId()==botonPunto.getId()) {
+
+            if(numeroActual.length()==0) numeroActual.setText("0.");
+
+            else if(!numeroActual.getText().toString().contains(".")) numeroActual.setText(numeroActual.getText() + ".");
+
+        }
+
+        if(esNumero(view) && faseActual==FasesCalculo.DESDE0) {
             numeroActual.setText(numeroActual.getText() + "" + textoBoton.getText());
             numero1 = Double.parseDouble((String) numeroActual.getText());
         }
 
-        if(esSigno(view)==true && numeroActual.getText()!="0") {
+        if(esSigno(view) && numeroActual.getText()!="0") {
 
             deshabilitarBotones(true);
             signo = (String) textoBoton.getText();
             faseActual = FasesCalculo.SEGUNDONUMERO;
+            botonPunto.setEnabled(false);
 
         }
 
-        if(esNumero(view)==true && numero2!=0 && faseActual==FasesCalculo.SEGUNDONUMERO) {
+        if(esNumero(view) && numero2!=0 && faseActual==FasesCalculo.SEGUNDONUMERO) {
+            botonPunto.setEnabled(true);
             numeroActual.setText(numeroActual.getText() + "" + textoBoton.getText());
             numero2 = Double.parseDouble((String) numeroActual.getText());
         }
 
-        else if(esNumero(view)==true && numero2==0 && faseActual==FasesCalculo.SEGUNDONUMERO) {
-            numeroActual.setText(textoBoton.getText());
-            numero2 = Double.parseDouble((String) numeroActual.getText());
+        else if(esNumero(view) && numero2==0 && faseActual==FasesCalculo.SEGUNDONUMERO) {
+            botonPunto.setEnabled(true);
+            if(numeroActual.getText().toString().contains("0.")) {
+                numeroActual.setText(numeroActual.getText() + "" + textoBoton.getText());
+                numero2 = Double.parseDouble((String) numeroActual.getText());
+            }
+            else {
+                numeroActual.setText(textoBoton.getText());
+                numero2 = Double.parseDouble((String) numeroActual.getText());
+            }
+
         }
+
+
 
         if(view.getId()==R.id.btnIgual && faseActual==FasesCalculo.SEGUNDONUMERO) {
 
             numero1 = operacionSigno(signo, numero1, numero2);
             double parteDecimal = numero1 % 1;
-            if(parteDecimal==0) numeroActual.setText(String.format("%.0f", numero1));
+            if(numero1==0) numeroActual.setText("");
+            else if(parteDecimal==0) numeroActual.setText(String.format("%.0f", numero1));
             else numeroActual.setText(numero1+"");
             numero2 = 0;
             faseActual = FasesCalculo.DESDE0;
@@ -164,10 +187,19 @@ public class MainActivity extends AppCompatActivity {
             borradoNumero = borradoNumero.substring(0, borradoNumero.length()-1);
             numeroActual.setText(borradoNumero);
             if(faseActual==FasesCalculo.DESDE0) {
-//coNTINUAR PORE AQUI
-                numero1= Double.parseDouble(String.valueOf(numeroActual));
+
+                if(borradoNumero.length()==0) numero1=0;
+
+                else numero1 = Double.parseDouble(borradoNumero);
 
             }
+
+            else {
+                if(borradoNumero.length()==0) numero2=0;
+
+                else numero2 = Double.parseDouble(borradoNumero);
+            }
+
         }
 
     }
